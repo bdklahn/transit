5df83ce0b03acf5df8fd2892bc5212da53b2477a
Jacob Porter <jsporter@virginia.edu>
2020-08-13 01:14:19 -0400

    Replaced identical zero with epsilon in statistics calculations.

--- src/pytransit/analysis/tn5gaps.py
+++ src/pytransit/analysis/tn5gaps.py
@@ -87,14 +87,14 @@ class Tn5GapsGUI(base.AnalysisGUI):
         tn5GapsLabel.SetFont( wx.Font( 10, wx.DEFAULT, wx.NORMAL, wx.BOLD) )
         tn5GapsSection.Add( tn5GapsLabel, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 
-        mainSizer1 = wx.BoxSizer( wx.VERTICAL )   
- 
+        mainSizer1 = wx.BoxSizer( wx.VERTICAL )
+
 
         # Min Read
         tn5GapsReadChoiceChoices = [ u"1", u"2", u"3", u"4", u"5" ]
         (tn5GapsReadLabel, self.wxobj.tn5GapsReadChoice, readSizer) = self.defineChoiceBox(tn5GapsPanel, u"Minimum Read:", tn5GapsReadChoiceChoices, "This is the minimum number of reads to consider a 'true' insertion. Value of 1 will consider all insertions. Larger values allow the method to ignore spurious insertions which might interrupt a run of non-insertions. Noisy datasets or those with many replicates can beneffit from increasing this.")
         mainSizer1.Add(readSizer, 1, wx.ALIGN_CENTER_HORIZONTAL|wx.EXPAND, 5 )
-   
+
 
         # Replicates
         tn5GapsRepChoiceChoices = [ u"Sum", u"Mean" ]
@@ -103,7 +103,7 @@ class Tn5GapsGUI(base.AnalysisGUI):
 
 
         tn5GapsSection.Add( mainSizer1, 1, wx.EXPAND, 5 )
- 
+
 
         tn5GapsButton = wx.Button( tn5GapsPanel, wx.ID_ANY, u"Run Tn5Gaps", wx.DefaultPosition, wx.DefaultSize, 0 )
         tn5GapsSection.Add( tn5GapsButton, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
@@ -112,7 +112,7 @@ class Tn5GapsGUI(base.AnalysisGUI):
         tn5GapsPanel.SetSizer( tn5GapsSection )
         tn5GapsPanel.Layout()
         tn5GapsSection.Fit( tn5GapsPanel )
-        
+
         #Connect events
         tn5GapsButton.Bind( wx.EVT_BUTTON, wxobj.RunMethod )
 
@@ -125,9 +125,9 @@ class Tn5GapsGUI(base.AnalysisGUI):
 ########## CLASS #######################
 
 class Tn5GapsMethod(base.SingleConditionMethod):
-    """   
+    """
     Example
- 
+
     """
     def __init__(self,
                 ctrldata,
@@ -179,7 +179,7 @@ class Tn5GapsMethod(base.SingleConditionMethod):
         #Get output path
         name = transit_tools.basename(ctrldata[0])
         defaultFileName = "tn5_gaps_output_m%d_r%s.dat" % (minread, replicates)
-        
+
 
         defaultDir = os.getcwd()
         output_path = wxobj.SaveFile(defaultDir, defaultFileName)
@@ -200,7 +200,7 @@ class Tn5GapsMethod(base.SingleConditionMethod):
                 CTerminus, wxobj)
 
     @classmethod
-    def fromargs(self, rawargs): 
+    def fromargs(self, rawargs):
         (args, kwargs) = transit_tools.cleanargs(rawargs)
 
         ctrldata = args[0].split(",")
@@ -233,9 +233,9 @@ class Tn5GapsMethod(base.SingleConditionMethod):
     def Run(self):
         self.transit_message("Starting Tn5 gaps method")
         start_time = time.time()
-        
+
         self.transit_message("Getting data (May take a while)")
-        
+
         # Combine all wigs
         (data,position) = transit_tools.get_validated_data(self.ctrldata, wxobj=self.wxobj)
         combined = tnseq_tools.combine_replicates(data, method=self.replicates)
@@ -243,12 +243,19 @@ class Tn5GapsMethod(base.SingleConditionMethod):
         counts = combined
         counts[counts > 0] = 1
         num_sites = counts.size
-        
-        genes_obj = tnseq_tools.Genes(self.ctrldata, self.annotation_path, ignoreCodon=self.ignoreCodon, nterm=self.NTerminus, cterm=self.CTerminus, data=data, position=position)
-        
-        pins = numpy.mean(counts)
-        pnon = 1.0 - pins
 
+        genes_obj = tnseq_tools.Genes(self.ctrldata, self.annotation_path, ignoreCodon=self.ignoreCodon, nterm=self.NTerminus, cterm=self.CTerminus, data=data, position=position)
+
+        pins = numpy.mean(counts)
+        eps = 2e-14
+        if pins == 0.0:
+            pins = eps
+            pnon = 1.0 - eps
+        elif pins == 1.0:
+            pins = 1.0 - eps
+            pnon = eps
+        else:
+            pnon = 1.0 - pins
         # Calculate stats of runs
         exprunmax = tnseq_tools.ExpectedRuns(num_sites, pnon)
         varrun = tnseq_tools.VarR(num_sites, pnon)
@@ -265,12 +272,12 @@ class Tn5GapsMethod(base.SingleConditionMethod):
         results_per_gene = {}
         for gene in genes_obj.genes:
             results_per_gene[gene.orf] = [gene.orf, gene.name, gene.desc, gene.k, gene.n, gene.r, 0, 0, 1]
-        
+
         N = len(run_arr)
         count = 0
         accum = 0
         self.progress_range(N)
-        for run in run_arr: 
+        for run in run_arr:
             accum += run['length']
             count += 1
             genes = tnseq_tools.get_genes_in_range(pos_hash, run['start'], run['end'])
@@ -288,20 +295,20 @@ class Tn5GapsMethod(base.SingleConditionMethod):
                 B = 1.0/math.log(1.0/pnon)
                 u = math.log(num_sites*pins, 1.0/pnon)
                 pval = 1.0 - tnseq_tools.GumbelCDF(run['length'], u, B)
-                
+
                 curr_val = results_per_gene[gene.orf]
                 curr_inter_sz = curr_val[6]
                 curr_len = curr_val[7]
                 if inter_sz > curr_inter_sz:
                     results_per_gene[gene.orf] = [gene.orf, gene.name, gene.desc, gene.k, gene.n, gene.r, inter_sz, run_len, pval]
-            
+
             # Update Progress
-            text = "Running Tn5Gaps method... %1.1f%%" % (100.0*count/N) 
+            text = "Running Tn5Gaps method... %1.1f%%" % (100.0*count/N)
             self.progress_update(text, count)
-                
+
         data = list(results_per_gene.values())
         exp_run_len = float(accum)/N
-        
+
         min_sig_len = float('inf')
         sig_genes_count = 0
         pval = [row[-1] for row in data]
@@ -313,7 +320,7 @@ class Tn5GapsMethod(base.SingleConditionMethod):
             data[i].append(padj[i]);
             data[i].append('Essential' if padj[i] < 0.05 else 'Non-essential');#(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4], data[i][5], data[i][6], data[i][7], data[i][8], padj[i], 'Essential' if padj[i] < 0.05 else 'Non-essential')
         data.sort(key=lambda l: l[0])
-            
+
         # Output results
         self.output.write("#Tn5 Gaps\n")
         if self.wxobj:
@@ -325,13 +332,13 @@ class Tn5GapsMethod(base.SingleConditionMethod):
         else:
             self.output.write("#Console: python3 %s\n" % " ".join(sys.argv))
 
-        self.output.write("#Data: %s\n" % (",".join(self.ctrldata).encode('utf-8'))) 
-        self.output.write("#Annotation path: %s\n" % self.annotation_path.encode('utf-8')) 
+        self.output.write("#Data: %s\n" % (",".join(self.ctrldata).encode('utf-8')))
+        self.output.write("#Annotation path: %s\n" % self.annotation_path.encode('utf-8'))
         self.output.write("#Time: %s\n" % (time.time() - start_time))
         self.output.write("#Essential gene count: %d\n" % (sig_genes_count))
         self.output.write("#Minimum reads: %d\n" % (self.minread))
         self.output.write("#Replicate combination method: %s\n" % (self.replicates))
-        self.output.write("#Minimum significant run length: %d\n" % (min_sig_len))
+        self.output.write("#Minimum significant run length: {}\n".format(min_sig_len))
         self.output.write("#Expected run length: %1.5f\n" % (exp_run_len))
         self.output.write("#Expected max run length: %s\n" % (exprunmax))
         self.output.write("#%s\n" % "\t".join(columns))
@@ -341,18 +348,18 @@ class Tn5GapsMethod(base.SingleConditionMethod):
             self.output.write("%s\t%s\t%s\t%s\t%s\t%s\t%d\t%d\t%1.5f\t%1.5f\t%s\n" % (res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8], res[9], res[10]))
         self.output.close()
 
-        self.transit_message("") # Printing empty line to flush stdout 
+        self.transit_message("") # Printing empty line to flush stdout
         self.transit_message("Adding File: %s" % (self.output.name))
         self.add_file(filetype="Tn5 Gaps")
         self.finish()
-        self.transit_message("Finished Tn5Gaps Method") 
+        self.transit_message("Finished Tn5Gaps Method")
 
 
     @classmethod
     def usage_string(self):
         #return """python3 %s resampling <comma-separated .wig control files> <comma-separated .wig experimental files> <annotation .prot_table or GFF3> <output file> [Optional Arguments]
         return """python3 %s tn5gaps <comma-separated .wig files> <annotation .prot_table or GFF3> <output file> [Optional Arguments]
-    
+
         Optional Arguments:
         -m <integer>    :=  Smallest read-count to consider. Default: -m 1
         -r <string>     :=  How to handle replicates. Sum or Mean. Default: -r Sum
@@ -361,7 +368,7 @@ class Tn5GapsMethod(base.SingleConditionMethod):
         """ % (sys.argv[0])
 
 
-        
+
 
 
 
@@ -371,7 +378,7 @@ class Tn5GapsMethod(base.SingleConditionMethod):
 
         if first[1] < second[0]:
             return 0
-        
+
         right_ovr = min(first[1], second[1])
         left_ovr = max(first[0], second[0])
 
@@ -381,10 +388,10 @@ class Tn5GapsMethod(base.SingleConditionMethod):
     def calc_overlap(self, run_interv, gene_interv):
         first = run_interv if run_interv[0] < gene_interv[0] else gene_interv
         second = run_interv if first == gene_interv else gene_interv
-        
+
         if second[0] > first[0] and second[1] < first[1]:
             return 1.0
-        
+
         intersect = self.intersect_size(run_interv, gene_interv)
         return float(intersect)/(gene_interv[1] - gene_interv[0])
 
@@ -397,7 +404,7 @@ if __name__ == "__main__":
 
     G = Tn5GapsMethod.fromargs(sys.argv[1:])
 
-    G.console_message("Printing the member variables:")   
+    G.console_message("Printing the member variables:")
     G.print_members()
 
     print("")
